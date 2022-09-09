c6c28f12039fb951182ddb4be9b5e74856217361
even.chang <even1101u@gmail.com>
2022-09-09 18:57:33 +0800

    Benchmark

--- src/api/utils/sort/sort_test.go
+++ src/api/utils/sort/sort_test.go
@@ -21,3 +21,34 @@ func TestBubbleSort(t *testing.T) {
 	}
 	fmt.Println(elements)
 }
+
+func TestSort(t *testing.T) {
+	// init
+	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	fmt.Println(elements)
+	// execution
+	Sort(elements)
+
+	// Validation
+	if elements[0] != 0 {
+		t.Error("first element should be 0")
+	}
+	if elements[len(elements)-1] != 9 {
+		t.Error("last element should be 9")
+	}
+	fmt.Println(elements)
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	for i := 0; i < b.N; i++ {
+		BubbleSort(elements)
+	}
+}
+
+func BenchmarkSort(b *testing.B) {
+	elements := []int{9, 7, 5, 3, 1, 2, 4, 6, 8, 0}
+	for i := 0; i < b.N; i++ {
+		Sort(elements)
+	}
+}
